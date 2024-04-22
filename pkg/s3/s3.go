f65b24ad6d69a0bf53129adc238a870ec58a3074
Daniel Brown <daniel.brown2@sainsburys.co.uk>
2024-04-22 12:33:57 +0100

    add func for generating presigned URL

--- pkg/s3/s3.go
+++ pkg/s3/s3.go
@@ -1,9 +1,12 @@
 package s3
 
 import (
+	"context"
 	"log"
+	"time"
 
 	"github.com/94DanielBrown/awsapp/pkg/awsconfig"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
@@ -16,11 +19,21 @@ func Connect() (*s3.Client, error) {
 	return client, nil
 }
 
+// GeneratePresignedURL creates a pre-signed URL for uploading an image to S3
+func GeneratePresignedURL(client *s3.Client, bucketName, key string, expiry time.Duration) (string, error) {
+	presigner := s3.NewPresignClient(client)
 
+	putObjectParams := &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(key),
+	}
 
+	resp, err := presigner.PresignPutObject(context.Background(), putObjectParams, func(p *s3.PresignOptions) {
+		p.Expires = expiry
+	})
+	if err != nil {
+		return "", err
+	}
 
-
-
-
-
+	return resp.URL, nil
 }
