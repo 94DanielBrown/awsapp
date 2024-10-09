bef1f7ec0fd6120b526dc580efdc83d0cfd4805e
Daniel Brown <daniel.brown2@sainsburys.co.uk>
2024-10-09 14:07:35 +0100

    add bucket owner full access control for objects uploaded with presignedURL
    Signed-off-by: Daniel Brown <daniel.brown2@sainsburys.co.uk>

--- pkg/s3/s3.go
+++ pkg/s3/s3.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"time"
 
-	"github.com/94DanielBrown/awsapp/pkg/awsconfig"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	s3types "github.com/aws/aws-sdk-go-v2/service/s3/types"
+
+	"github.com/94DanielBrown/awsapp/pkg/awsconfig"
 )
 
 const (
@@ -37,11 +39,13 @@ func (c *Client) GeneratePresignedURL(bucketName, key string, contentType string
 		return "", fmt.Errorf("invalid content type: %s", contentType)
 	}
 	presigner := s3.NewPresignClient(c.S3)
+	acl := s3types.ObjectCannedACLBucketOwnerFullControl
 
 	putObjectParams := &s3.PutObjectInput{
 		Bucket:      aws.String(bucketName),
 		Key:         aws.String(key),
 		ContentType: aws.String(contentType),
+		ACL:         acl,
 	}
 
 	resp, err := presigner.PresignPutObject(context.Background(), putObjectParams, func(p *s3.PresignOptions) {
